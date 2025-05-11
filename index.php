ad2db3c57139bf018ca802fb9734f371b88bafd0
Fernando <fernando93d@gmail.com>
2025-05-10 19:51:27 -0600

    Init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Masterclass GitHub Actions en CodigoFacilito</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+  
+  <img src="https://codigofacilito.com/assets/cody-with-techs-0a16265a54871f5278e952befe15f476b32c06bae4f46fb9df867e3e2f73b4ba.png" 
+    style="width: 500px;" class="w-72 h-auto mb-4 rounded shadow-lg" />
+
+  <p class="text-xl text-gray-700 font-semibold" style="font-size: 30px;">
+    Masterclass GitHub Actions en CodigoFacilito
+  </p>
+
+</body>
+</html>
