bf7280f7c9fd59649c9c9ba499f31d496200a46b
Fernando <fernando93d@gmail.com>
2025-05-10 19:51:27 -0600

    Init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Imagen centrada</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+  
+  <img src="./cody2.png" style="width: 500px;" class="w-72 h-auto mb-4 rounded shadow-lg" />
+
+  <p class="text-xl text-gray-700 font-semibold" style="font-size: 30px;">
+    Masterclass GitHub Actions en CodigoFacilito
+  </p>
+
+</body>
+</html>
